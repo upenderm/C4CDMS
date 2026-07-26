c331a0986cc4cc1572d15bef807fb46883d7aa4b
Upender Mamindlapelly <Upender0209@gmail.com>
2026-07-26 17:02:47 -0400

    Bug fixes and after OCI setup

--- cdmsapp/src/app/services/contracts.service.ts
+++ cdmsapp/src/app/services/contracts.service.ts
@@ -4,6 +4,7 @@ import { ContractDocument } from '../common/contract-document';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { CookieService } from 'ngx-cookie-service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,8 @@ import { CookieService } from 'ngx-cookie-service';
 export class ContractsService {
 
   contractsList: ContractDocument[] = [];
-  private cmsAPITarget: string = 'http://localhost:8082/cdmsapi/';
-  private allAvailableContractsPath: string = "contractDocs/allAvailableContracts";
+  private cmsAPITarget: string = environment.apiUrl;
+  private allAvailableContractsPath: string = "/contractDocs/allAvailableContracts";
 
   constructor(
     private http: HttpClient,
@@ -21,6 +22,7 @@ export class ContractsService {
 
   getListOfAvailableContracts(companyOID: string): Observable<any> {
     this.cookieService.get('C4Token')
+    console.log('API URL :'+environment.apiUrl);
     return this.http.get(this.cmsAPITarget + this.allAvailableContractsPath, {
       headers: this.createAuthorizationHeader(companyOID)
     })
